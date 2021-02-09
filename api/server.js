b52febb204f4ed02fe61a34204c0b1780bb41b23
ndossett <niki.m.dossett@gmail.com>
2021-02-08 21:30:40 -0500

    set up port, express and generate

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const generate = require('shortid').generate;
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+let user = [
+    {id:generate(), name:'Janette Doe', bio: 'School Student'},
+    {id:generate(), name:'Johnny Doe', bio: 'School Teacher'}
+]
+
+console.log(user)
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// import { server } from './api/server';
+
 const server = require('./api/server');
 
-const port = 5000;
+// const http = require('http');
+
+const PORT = 5000;
 
 // START YOUR SERVER HERE
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
